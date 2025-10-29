068c0e7cbf32981a8eccded7804873390cd865d6
joeCQ2000 <joelcamargoquispe4asec@gmail.com>
2025-10-29 16:23:39 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/inmobivalor_be/InmobivalorBeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.inmobivalor_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmobivalorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InmobivalorBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/inmobivalor_be/InmobivalorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.inmobivalor_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobivalorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
