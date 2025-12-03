8a359b62b4c594b9f03ca107047af39e05bacdc7
Akio <u202218649@upc.edu.pe>
2025-12-02 19:44:28 -0500

    Commit autoregistro de Rol "Asesor financiero"

--- src/main/java/pe/edu/upc/inmobivalor_be/controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/inmobivalor_be/controllers/UsuarioController.java
@@ -6,9 +6,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.inmobivalor_be.dtos.UsuarioDTO;
+import pe.edu.upc.inmobivalor_be.entities.Rol;
 import pe.edu.upc.inmobivalor_be.entities.Usuario;
 import pe.edu.upc.inmobivalor_be.serviceinterfaces.IUsuarioService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,15 +38,21 @@ public class UsuarioController {
             return modelMapper.map(x, Usuario.class);
         }).collect(Collectors.toList());
     }
+
     @PostMapping("/registrar")
-    //@PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void registrar (@RequestBody UsuarioDTO usuarioDTO) {
         ModelMapper m = new ModelMapper();
         Usuario usuario = m.map(usuarioDTO, Usuario.class);
         usuario.setContrasenha(passwordEncoder.encode(usuario.getContrasenha()));
+        Rol rolDefault = new Rol();
+        rolDefault.setNombreRol("ASESOR_FINANCIERO");
+        rolDefault.setEstado(true);
+        rolDefault.setUser(usuario);
+        List<Rol> roles = new ArrayList<>();
+        roles.add(rolDefault);
+        usuario.setRoles(roles);
         usuarioService.insert(usuario);
     }
-
 
     @PutMapping("/actualizar")
     public void actualizar(@RequestBody UsuarioDTO dto) {
